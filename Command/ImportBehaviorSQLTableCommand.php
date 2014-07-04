11fd24752f8a72cde07fb5b0f7d65333f00dadbd
papepapes <papepapes@gmail.com>
2014-07-04 19:32:21 +0000

    Command updated. Droped Connection option

--- Command/ImportBehaviorSQLTableCommand.php
+++ Command/ImportBehaviorSQLTableCommand.php
@@ -19,55 +19,49 @@ use Doctrine\ORM\Query\ResultSetMapping;
  */
 class ImportBehaviorSQLTableCommand extends ContainerAwareCommand
 {
-    
+
     protected function configure()
     {
         $this
-            ->setName('paco:followable:create-table')
-            ->setDescription('Create the followable behavior required table.')
-            ->addOption('dump-sql', null, InputOption::VALUE_OPTIONAL, 'Display the SQL this bundle trun against your database.')
-            ->addOption('connection', null, InputOption::VALUE_OPTIONAL, 'The name of the connection you want to use.')
-            ->setHelp(<<<EOT
-The <info>paco:followable:create-table</info> command creates the joint table used to setup the many-to-many relationship:
+        ->setName('paco:followable:create-table')
+        ->setDescription('Create the followable behavior required table.')
+        ->addOption('dump-sql', null, InputOption::VALUE_OPTIONAL, 'Display the SQL this bundle trun against your database.')
+        ->setHelp(<<<EOT
+            The <info>paco:followable:create-table</info> command creates the joint table used to setup the many-to-many relationship:
 
-<info>php app/console paco:followable:create-table</info>
+            <info>php app/console paco:followable:create-table</info>
 
 
-You can also optionally specify the name of a connection to drop the database
-for:
+            You can also optionally tell this command to dump the SQL it runs on your database:
 
-<info>php app/console paco:followable:create-table --connection=default</info>
+            <info>php app/console paco:followable:create-table --dump-sql</info>
 
-And tell this command to dump the SQL it runs on your connection:
+            <error>Be careful: this command drop the joint table if it exists before creating it.</error>
+            EOT
+            );
+}
 
-<info>php app/console paco:followable:create-table --dump-sql</info>
+protected function execute(InputInterface $input, OutputInterface $output)
+{
+    $rsm = new ResultSetMapping();
+    $em = $this->getContainer()->get('doctrine')->getManager();
+    $users_table_name = $this->getContainer()->getParameter('users_table_name');
+    $followable_id_attribute_name = $this->getContainer()->getParameter('followable_id_attribute_name');
+    $joint_table_name = $this->getContainer()->getParameter('joint_table_name');
+    $joint_table_follower_column_name = $this->getContainer()->getParameter('joint_table_follower_column_name');
+    $joint_table_followee_column_name = $this->getContainer()->getParameter('joint_table_followee_column_name');
+    $sql = '<<<EOT 
+    --
+    -- Structure de la table `follows`
+    --
 
-<error>Be careful: this command drop the joint table if it exists before creating it.</error>
-EOT
-        );
-    }
-
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
-        $rsm = new ResultSetMapping();
-        $em = $this->getContainer()->get('doctrine')->getManager();
-        $users_table_name = $this->getContainer()->getParameter('users_table_name');
-        $followable_id_attribute_name = $this->getContainer()->getParameter('followable_id_attribute_name');
-        $joint_table_name = $this->getContainer()->getParameter('joint_table_name');
-        $joint_table_follower_column_name = $this->getContainer()->getParameter('joint_table_follower_column_name');
-        $joint_table_followee_column_name = $this->getContainer()->getParameter('joint_table_followee_column_name');
-        $sql = '<<<EOT 
---
--- Structure de la table `follows`
---
-
-DROP TABLE IF EXISTS `'.$joint_table_name.'`;
-CREATE TABLE IF NOT EXISTS `follows` (
-  `'.$joint_table_follower_column_name.'` int(11) NOT NULL,
-  `'.$joint_table_followee_column_name.'` int(11) NOT NULL,
-  KEY `'.$joint_table_follower_column_name.'` (`'.$joint_table_follower_column_name.'`),
-  KEY `'.$joint_table_followee_column_name.'` (`'.$joint_table_followee_column_name.'`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+    DROP TABLE IF EXISTS `'.$joint_table_name.'`;
+    CREATE TABLE IF NOT EXISTS `follows` (
+      `'.$joint_table_follower_column_name.'` int(11) NOT NULL,
+      `'.$joint_table_followee_column_name.'` int(11) NOT NULL,
+      KEY `'.$joint_table_follower_column_name.'` (`'.$joint_table_follower_column_name.'`),
+      KEY `'.$joint_table_followee_column_name.'` (`'.$joint_table_followee_column_name.'`)
+      ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contraintes pour les tables exportées
@@ -77,15 +71,16 @@ CREATE TABLE IF NOT EXISTS `follows` (
 -- Contraintes pour la table `follows`
 --
 ALTER TABLE `follows`
-  ADD CONSTRAINT `followee_user_fk` FOREIGN KEY (`'.$joint_table_followee_column_name.'`) REFERENCES `'.$users_table_name.'` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `follower_user_fk` FOREIGN KEY (`'.$joint_table_follower_column_name.'`) REFERENCES `'.$users_table_name.'` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ADD CONSTRAINT `followee_user_fk` FOREIGN KEY (`'.$joint_table_followee_column_name.'`) REFERENCES `'.$users_table_name.'` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `follower_user_fk` FOREIGN KEY (`'.$joint_table_follower_column_name.'`) REFERENCES `'.$users_table_name.'` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 EOT        
 ';
-        $cnx->executeUpdate('');
-        if($input->hasOption('dump-sql')){
-            $output->writeln($sql);
-        }
+$cnx = $em->getConnection();
+$cnx->exec($sql);
+if($input->hasOption('dump-sql')){
+    $output->writeln($sql);
+}
 
-    }
+}
 }
